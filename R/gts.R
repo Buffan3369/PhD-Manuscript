baaa0c2c3e3f0f34f679759fd5d839f221cc5d92
Lucas Buffan <94531347+Buffan3369@users.noreply.github.com>
2026-05-09 15:47:17 +0200

    stratum 1. Done

--- R/gts.R
+++ R/gts.R
@@ -6,6 +6,7 @@ source("../Chinchilloids/R/useful/load_GTS.R")
 x <- 0:66
 y <- rnorm(n = 67)
 
+## Horizontal ------------------------------------------------------------------
 p <- ggplot(data = data.frame(x, y), aes(x = x, y = y)) +
   geom_point() +
   scale_x_reverse(breaks = seq(0, 60, 10)) +
@@ -14,4 +15,15 @@ p <- ggplot(data = data.frame(x, y), aes(x = x, y = y)) +
   theme(axis.title = element_text(size = 12))
 
 ggsave("./Figures/SAM_simplified_chronogram/geoscale_plot.pdf", plot = p, height = 100, width = 400, units = "mm")
-View(deeptime::stages)
+
+## Vertical --------------------------------------------------------------------
+
+p_vert <- ggplot(data = data.frame(x, y), aes(x = y, y = x)) +
+  geom_point() +
+  scale_y_reverse(breaks = seq(0, 60, 10)) +
+  labs(y = "Time (Ma)") +
+  coord_geo(pos = list("left", "left"), dat = list(epochs, gsc3), abbrv = list(F, F), size = list(4, 3), height = unit(6, "line")) +
+  theme(axis.title = element_text(size = 12))
+
+ggsave("./Figures/GTS_plot/vertical_gts.pdf", plot = p_vert, height = 240, width = 200, units = "mm")
+  
