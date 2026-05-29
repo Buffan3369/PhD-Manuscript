9ce26b36a5bd5f3ba9183925a34227683c8ba121
Lucas Buffan <94531347+Buffan3369@users.noreply.github.com>
2026-05-29 12:49:06 +0200

    incorporate PierrO's comments

---
+++ R/parse_references.R
@@ -0,0 +1,25 @@
+uppercase <- function(letter){
+  if(letter %in% LETTERS){
+    return(paste0("{", letter, "}"))
+  }
+  else{
+    return(letter)
+  }
+}
+
+biblio <- readLines("./references.bib")
+for(i in 1:length(biblio)){
+  line <- biblio[i]
+  if(line != ""){
+    spl_equal <- strsplit(line, split = " = ")[[1]]
+    if(spl_equal[1] == "\ttitle"){
+      ttl <- spl_equal[2]
+      ttl_upr <- paste0(sapply(strsplit(ttl, split = "")[[1]],
+                               FUN = uppercase), 
+                        collapse = "")
+      biblio[i] <- paste0(spl_equal[1], " = ", ttl_upr)
+    }
+  }
+}
+
+writeLines(biblio, "./references1.bib")
